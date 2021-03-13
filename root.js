27356f0ee77a92b4021a11e5f3884bb070db8738
Sandile <Sandilandz14>
2021-03-13 23:58:49 +0200

    Added updated files

---
+++ root.js
@@ -0,0 +1,14 @@
+   // typing text animation script
+   var typed = new Typed(".typing", {
+    strings: ["Software Developer", "Web Developer", "Programmer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
+
+var typed = new Typed(".typing-2", {
+    strings: ["Software Developer", "Web Developer", "Programmer"],
+    typeSpeed: 100,
+    backSpeed: 60,
+    loop: true
+});
