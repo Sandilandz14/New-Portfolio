0bfd30eb54dca28bad103b713c0317a0c5495dd6
Sandile <Sandilandz14>
2021-03-14 04:07:27 +0200

    Updated

--- root.js
+++ root.js
@@ -12,3 +12,35 @@ var typed = new Typed(".typing-2", {
     backSpeed: 60,
     loop: true
 });
+
+
+// BUILDING FUNCTION FOR ONCLICK ANIMATION
+let barOne = document.getElementsByClassName('bar')[0],
+$boxTwo = $('.box:eq(1)');
+
+document.getElementsByClassName('skills')[0].onclick = function() {
+if(this.innerHTML === 'Play') 
+{ 
+this.innerHTML = 'Pause';
+boxOne.classList.add('horizTranslate');
+  } else {
+    this.innerHTML = 'Play';
+    var computedStyle = window.getComputedStyle(boxOne),
+        marginLeft = computedStyle.getPropertyValue('margin-left');
+    boxOne.style.marginLeft = marginLeft;
+    boxOne.classList.remove('horizTranslate');    
+  }  
+}
+
+$('.toggleButton:eq(1)').on('click', function() { 
+  if($(this).html() === 'Play') 
+  {
+    $(this).html('Pause');
+    $boxTwo.addClass('horizTranslate');
+  } else {
+    $(this).html('Play');
+    var computedStyle = $boxTwo.css('margin-left');
+    $boxTwo.removeClass('horizTranslate');
+    $boxTwo.css('margin-left', computedStyle);
+  }  
+});
